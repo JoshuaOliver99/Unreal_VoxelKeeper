f866d939189cf40f7bf1236778f45567b991863e
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2023-02-06 13:05:35 +0000

    Project Init + Rider + Starter Content

---
+++ VoxelKeeper/Source/VoxelKeeper/VoxelKeeperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelKeeperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELKEEPER_API AVoxelKeeperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
